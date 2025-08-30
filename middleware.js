b811f981f241ca886bf65929f5fa72a5e1e7a495
DARSHAN973 <darshannichite04@gmail.com>
2025-08-30 19:52:08 +0530

    trying to deploy on vercel try 2

--- middleware.js
+++ middleware.js
@@ -1,14 +1,11 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-	try {
-		console.log('[middleware] ', request.nextUrl.pathname);
-	} catch (e) {
-
-	}
-
-	return NextResponse.next();
+  // If you need to check URL, you can still access:
+  // request.nextUrl.pathname
+  return NextResponse.next();
 }
+
 export const config = {
-	matcher: ['/:path*']
+  matcher: ['/:path*'],
 };
