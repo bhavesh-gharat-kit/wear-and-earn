3e5d23916e8fa9bacc5f3a1ef0a673073a88833c
bhavesh-gharat-kit <bhavesh.gharat.kit@gmail.com>
2025-10-17 06:59:14 +0000

    products spaces removed

--- app/navigations/product-details-page/ProductDetailsPage.jsx
+++ app/navigations/product-details-page/ProductDetailsPage.jsx
@@ -131,8 +131,8 @@ function ProductDetailsPage({ id }) {
   };
 
   return (
-  <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
-  <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+  <div className="min-h-screen">
+  <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 py-8">
         {/* Main Product Section */}
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12 mb-12">
           {/* Product Images */}
